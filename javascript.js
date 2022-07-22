9bdade7bcfaccc2b08fde1cb6e4294f7f2386197
Bryan <bryanlohjw97@gmail.com>
2022-07-23 01:18:49 +0800

    add color change function for block

--- javascript.js
+++ javascript.js
@@ -1,15 +1,24 @@
 //creation of DOM nodes
 const topContainer = document.createElement('div');
 for (let i = 0; i < 4; i++) {
-    const div = document.createElement('div');
-    div.classList.add('row');
-    div.setAttribute('id', `row${i}`);
+    const divRow = document.createElement('div');
+    divRow.classList.add('row');
+    divRow.setAttribute('id', `row${i}`);
     for (let k = 0; k < 4; k++) {
-        const block = document.createElement('div');
-        block.classList.add('block');
-        block.setAttribute('id', `block${i}${k}`);
-        div.appendChild(block);
+        const blockCreater = document.createElement('div');
+        blockCreater.classList.add('block');
+        blockCreater.setAttribute('id', `block${i}${k}`);
+        divRow.appendChild(blockCreater);
     }
-    topContainer.appendChild(div);
+    topContainer.appendChild(divRow);
 }
 document.body.appendChild(topContainer);
+
+//setting event listener for hover
+const blocks = document.querySelectorAll('.block');
+blocks.forEach(block => {
+    block.addEventListener('mouseover',(e) => {
+        e.target.classList.toggle('black');
+        console.log(e.target);
+    })
+});
